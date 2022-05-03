bec90bc07c355c752c5b7fd2e45e0e625f444dab
kmoussa <konemoussamohamed@gmail.com>
2022-05-03 09:15:24 +0200

    Initial commit

---
+++ src/main/java/cc/controleur/Controleur.java
@@ -0,0 +1,15 @@
+package cc.controleur;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class Controleur {
+
+    // TODO compléter le contrôleur
+   
+
+}
